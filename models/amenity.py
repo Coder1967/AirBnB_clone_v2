3ee4a1cf7140546e9f0e9c5298ee1299e69a8dfa
Aisosa <allysonaghedo@gmail.com>
2022-10-12 23:48:22 +0100

    fixing errors

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, Table
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -12,5 +13,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary='place_amenity',
-                                   back_populates='amenities')
+    place_amenities = relationship("Place", secondary=place_amenity)
