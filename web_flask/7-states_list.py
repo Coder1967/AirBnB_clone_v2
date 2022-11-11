e5d881aca9ebfc78340e8510348a5900e4246747
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-11 22:14:47 +0100

    finishing up another task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,15 +5,17 @@ from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/states_list', strict_slashes=False)
 def list_state():
     """ returns a list of states"""
     states = storage.all(State)
-    return render_template(7-states_list.html, states=states)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def tear_down():
+def tear_down(ex):
     """ ran after every request. reloads
     the storage engine"""
     storage.close()
