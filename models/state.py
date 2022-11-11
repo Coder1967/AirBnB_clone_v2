43e62eeffea13b36233413f0786ce7480d64d17a
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-11-11 11:56:54 +0000

    fixing errors

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
+import models
+from models.city import City
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -18,7 +20,7 @@ class State(BaseModel, Base):
         def cities(self):
             """getter for list of city instances related to the state"""
             city_list = []
-            all_cities = storage.all(City)
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
