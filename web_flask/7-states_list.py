c7411d35c93c8ce7ced0ec65e11ee4cfddf77311
nosakhare aghedo <nosakhareaghedo42@gmail.com>
2022-11-11 21:55:19 +0100

    finishing up another task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" sets up flask website"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/states_list', strict_slashes=False)
+def list_state():
+    """ returns a list of states"""
+    states = storage.all(State)
+    return render_template(7-states_list.html, states=states)
+
+
+@app.teardown_appcontext
+def tear_down():
+    """ ran after every request. reloads
+    the storage engine"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
