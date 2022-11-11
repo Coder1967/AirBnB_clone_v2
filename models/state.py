3090db4d2c4dcaef47fc54b47bba121ff4eb2d7d
Nosakhare Aghedo <nosakhareaghedo42@gmail.com>
2022-11-11 18:57:17 +0000

    fixing up

--- models/state.py
+++ models/state.py
@@ -13,14 +13,15 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        """getter for list of city instances related to the state"""
-        city_list = []
-        all_cities = storage.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            '''getter for list of city instances related to the state'''
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
